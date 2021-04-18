fd37f8c81499a915577bf13c8d2683de201f6142
Oriya Shrestha <oriya.shrestha111@gmail.com>
2021-04-18 23:39:39 +0545

    miniproject

--- greeting/src/index.js
+++ greeting/src/index.js
@@ -1,11 +1,36 @@
 import react from "react";
 import ReactDOM from "react-dom";
+import './index.css';
 
-let curDate = new Date();
-curDate = curDate.getTime();
 
+
+let curDate = new Date(2021,4,18,21);
+curDate = curDate.getHours();
+
+let greet = '';
+const cssStyle= {
+    color:"",
+    Width:""
+};
+
+if (curDate>=1 && curDate<12) {
+    greet="Good Morning.";
+    cssStyle.color="green";
+} else if(curDate>=12 && curDate<=19){
+    greet="Good Afternoon.";
+    cssStyle.color="Orange";
+} else{
+    greet="Good Evening.";
+    cssStyle.color="Brown";
+}
 
 ReactDOM.render(
-    <h1>Hello Sir, Good Morning!. Its currently {curDate}</h1>,
-    document.getElementById('root')
+    <>
+    <div style={{Width:"50px"}}>
+    <h1>
+        Hello Sir! <span style={cssStyle}>{greet} </span>
+    </h1>
+    </div>
+    </>
+    ,document.getElementById('root')
 );
