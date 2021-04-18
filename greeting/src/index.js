5ae7cc6373ece09cd086ebaa95426ad48f3db302
Oriya Shrestha <oriya.shrestha111@gmail.com>
2021-04-18 19:35:14 +0545

    next commit

--- greeting/src/index.js
+++ greeting/src/index.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import react from "react";
+import ReactDOM from "react-dom";
+
+let curDate = new Date();
+curDate = curDate.getTime();
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <h1>Hello Sir, Good Morning!. Its currently {curDate}</h1>,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
